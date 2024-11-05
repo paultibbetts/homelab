ccc536fa3982de9bea21e19f4be619174c1f452c
Paul Tibbetts <code@paultibbetts.uk>
2024-11-05 21:45:03 +0000

    feat: use S3 for .tfstate (#22)
    Reviewed-on: http://192.168.1.211:3000/paul/homelab/pulls/22
    
    seems to work on my Macbook but that has an empty state, so pushing to test from Ubuntu which has an active state file that I don't want to mess with.
    Co-authored-by: Paul Tibbetts <code@paultibbetts.uk>
    Co-committed-by: Paul Tibbetts <code@paultibbetts.uk>

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,6 +5,16 @@ terraform {
       version = "2.9.14"
     }
   }
+  backend "s3" {
+    bucket                      = "tfstate"
+    key                         = "homelab.tfstate"
+    region                      = "main" # region validation will be skipped
+    skip_credentials_validation = true   # Skip AWS related checks and validations
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    use_path_style              = true # Enable path-style S3 URLs (https://<HOST>/<BUCKET> https://developer.hashicorp.com/terraform/language/settings/backends/s3#use_path_style
+  }
 }
 
 provider "proxmox" {
