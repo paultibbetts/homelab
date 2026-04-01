4ca280719e23d3249112ecc6ded242a4378b0334
Paul Tibbetts <code@paultibbetts.uk>
2026-04-01 21:56:03 +0100

    refactor: minio endpoint

--- external/secrets.sh
+++ external/secrets.sh
@@ -1,3 +1,3 @@
-export AWS_ENDPOINT_URL_S3=http://192.168.1.4:9000/
+export AWS_ENDPOINT_URL_S3=$(pass minio_endpoint)
 export AWS_ACCESS_KEY_ID=$(pass minio_access_key)
 export AWS_SECRET_ACCESS_KEY=$(pass minio_secret_key)
